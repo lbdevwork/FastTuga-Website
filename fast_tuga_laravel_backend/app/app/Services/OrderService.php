f649959a4478adc1e6e5f692ce2e1e599ed032be
pjclara <pjclara@gmail.com>
2022-12-05 15:01:58 +0000

    update

--- fast_tuga_laravel_backend/app/app/Services/OrderService.php
+++ fast_tuga_laravel_backend/app/app/Services/OrderService.php
@@ -7,30 +7,6 @@ use App\Models\Order;
 class OrderService
 {
 
-    public function store(array $orderData): Order
-    {
-        $order = Order::create($orderData);
-
-
-        return $order;
-    }
-
-    public function update(array $orderData, Order $order): Order
-    {
-        $order->update($orderData);
-
-        return $order;
-    }
-}
-<?php
-
-namespace App\Services;
-
-use App\Models\Order;
-
-class OrderService
-{
-
     public function store(array $data): Order
     {
         // criar o ticket number 0-99
